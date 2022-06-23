a6fbf8d6bb3e96bc836dd3e62564280a36ae228f
M.D. Walters <808426@pdsb.net>
2022-06-23 13:44:13 -0400

    Make icons less exclusive to Melt's laptop

--- meltbrowser.py
+++ meltbrowser.py
@@ -27,7 +27,7 @@ class AboutDialog(QDialog):
         layout.addWidget(title)
 
         logo = QLabel()
-        logo.setPixmap(QPixmap('/home/sus/meltbrowser/images/ma-icon-128.png'))
+        logo.setPixmap(QPixmap('~/meltbrowser/images/ma-icon-128.png'))
         layout.addWidget(logo)
 
         layout.addWidget(QLabel("Version 1.0.0.0.0"))
@@ -61,27 +61,27 @@ class MainWindow(QMainWindow):
         navtb.setIconSize(QSize(16, 16))
         self.addToolBar(navtb)
 
-        back_btn = QAction(QIcon('/home/sus/meltbrowser/images/arrow-180.png'), "Back", self)
+        back_btn = QAction(QIcon('~/meltbrowser/images/arrow-180.png'), "Back", self)
         back_btn.setStatusTip("Go back")
         back_btn.triggered.connect(lambda: self.tabs.currentWidget().back())
         navtb.addAction(back_btn)
 
-        next_btn = QAction(QIcon('/home/sus/meltbrowser/images/arrow-000.png'), "Forward", self)
+        next_btn = QAction(QIcon('~/meltbrowser/images/arrow-000.png'), "Forward", self)
         next_btn.setStatusTip("Go forward")
         next_btn.triggered.connect(lambda: self.tabs.currentWidget().forward())
         navtb.addAction(next_btn)
 
-        reload_btn = QAction(QIcon('/home/sus/meltbrowser/images/arrow-circle-315.png'), "Reload", self)
+        reload_btn = QAction(QIcon('~/meltbrowser/images/arrow-circle-315.png'), "Reload", self)
         reload_btn.setStatusTip("Reload page")
         reload_btn.triggered.connect(lambda: self.tabs.currentWidget().reload())
         navtb.addAction(reload_btn)
 
-        home_btn = QAction(QIcon('/home/sus/meltbrowser/images/home.png'), "Home", self)
+        home_btn = QAction(QIcon('~/meltbrowser/images/home.png'), "Home", self)
         home_btn.setStatusTip("Go home")
         home_btn.triggered.connect(self.navigate_home)
         navtb.addAction(home_btn)
         
-        new_tab_action = QAction(QIcon('/home/sus/meltbrowser/images/new-tab.png'), "New Tab", self)
+        new_tab_action = QAction(QIcon('~/meltbrowser/images/new-tab.png'), "New Tab", self)
         new_tab_action.setStatusTip("Open new tab")
         new_tab_action.triggered.connect(lambda _: self.add_new_tab())
         navtb.addAction(new_tab_action)
@@ -89,14 +89,14 @@ class MainWindow(QMainWindow):
         navtb.addSeparator()
 
         self.httpsicon = QLabel()  # Yes, really!
-        self.httpsicon.setPixmap(QPixmap('/home/sus/meltbrowser/images/lock-nossl.png'))
+        self.httpsicon.setPixmap(QPixmap('~/meltbrowser/images/lock-nossl.png'))
         navtb.addWidget(self.httpsicon)
 
         self.urlbar = QLineEdit()
         self.urlbar.returnPressed.connect(self.navigate_to_url)
         navtb.addWidget(self.urlbar)
 
-        stop_btn = QAction(QIcon('/home/sus/meltbrowser/images/cross-circle.png'), "Stop", self)
+        stop_btn = QAction(QIcon('~/meltbrowser/images/cross-circle.png'), "Stop", self)
         stop_btn.setStatusTip("Stop reloading")
         stop_btn.triggered.connect(lambda: self.tabs.currentWidget().stop())
         navtb.addAction(stop_btn)
@@ -106,29 +106,29 @@ class MainWindow(QMainWindow):
 
         file_menu = self.menuBar().addMenu("&File")
 
-        open_file_action = QAction(QIcon('/home/sus/meltbrowser/images/disk--arrow.png'), "Open file...", self)
+        open_file_action = QAction(QIcon('~/meltbrowser/images/disk--arrow.png'), "Open file...", self)
         open_file_action.setStatusTip("Open file")
         open_file_action.triggered.connect(self.open_file)
         file_menu.addAction(open_file_action)
 
-        save_file_action = QAction(QIcon('/home/sus/meltbrowser/images/disk--pencil.png'), "Save Page As...", self)
+        save_file_action = QAction(QIcon('~/meltbrowser/images/disk--pencil.png'), "Save Page As...", self)
         save_file_action.setStatusTip("Save current page as file")
         save_file_action.triggered.connect(self.save_file)
         file_menu.addAction(save_file_action)
 
-        print_action = QAction(QIcon('/home/sus/meltbrowser/images/printer.png'), "Print...", self)
+        print_action = QAction(QIcon('~/meltbrowser/images/printer.png'), "Print...", self)
         print_action.setStatusTip("Print current page")
         print_action.triggered.connect(self.print_page)
         file_menu.addAction(print_action)
 
         help_menu = self.menuBar().addMenu("&Help")
 
-        about_action = QAction(QIcon('/home/sus/meltbrowser/images/question.png'), "About MeltBrowser", self)
+        about_action = QAction(QIcon('~/meltbrowser/images/question.png'), "About MeltBrowser", self)
         about_action.setStatusTip("Find out more about MeltBrowser")  # Hungry!
         about_action.triggered.connect(self.about)
         help_menu.addAction(about_action)
 
-        navigate_mozarella_action = QAction(QIcon('/home/sus/meltbrowser/images/lifebuoy.png'),
+        navigate_mozarella_action = QAction(QIcon('~/meltbrowser/images/lifebuoy.png'),
                                             "MeltBrowser Homepage", self)
         navigate_mozarella_action.setStatusTip("Go to MeltBrowser Homepage")
         navigate_mozarella_action.triggered.connect(self.navigate_mozarella)
@@ -139,7 +139,7 @@ class MainWindow(QMainWindow):
         self.show()
 
         self.setWindowTitle("MeltBrowser")
-        self.setWindowIcon(QIcon('/home/sus/meltbrowser/images/ma-icon-64.png'))
+        self.setWindowIcon(QIcon('~/meltbrowser/images/ma-icon-64.png'))
 
     def add_new_tab(self, qurl=None, label="New Tab"):
 
@@ -235,11 +235,11 @@ class MainWindow(QMainWindow):
 
         if q.scheme() == 'https':
             # Secure padlock icon
-            self.httpsicon.setPixmap(QPixmap('/home/sus/meltbrowser/images/lock-ssl.png'))
+            self.httpsicon.setPixmap(QPixmap('~/meltbrowser/images/lock-ssl.png'))
 
         else:
             # Insecure padlock icon
-            self.httpsicon.setPixmap(QPixmap('/home/sus/meltbrowser/images/lock-nossl.png'))
+            self.httpsicon.setPixmap(QPixmap('~/meltbrowser/images/lock-nossl.png'))
 
         self.urlbar.setText(q.toString())
         self.urlbar.setCursorPosition(0)
